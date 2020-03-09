e1879ae591045927ee191c9e2d64135307b2b9cc
juan <juansetonog@gmail.com>
2020-03-09 15:23:42 -0500

    actualizacion

--- Pets/app/src/main/java/com/example/pets/LoginActivity.java
+++ Pets/app/src/main/java/com/example/pets/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.pets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,4 +14,16 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void btn_Login(View view){
+        Toast.makeText(this, "Your have pressed Login Button", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(  this,  InboxActivity.class);
+        startActivity(intent);
+    }
+    public void btn_Signup(View view){
+        Toast.makeText(this, "Your have pressed Sign up Button", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(  this,  InboxActivity.class);
+        startActivity(intent);
+    }
+
 }
